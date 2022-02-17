529c80580bc83cae0d94de2f253ed839df433d33
rrogers422 <Reagan422440@gmail.com>
2022-02-17 17:18:52 -0500

    added bootstrap icons on hopmepage

--- -a/src/App.js
+++ -a/src/App.js
@@ -1,10 +1,11 @@
-
+import Profile from './PortfolioContainer/Home/Profile';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Profile>
+</Profile>
     </div>
   );
 }
