c08c93e8efcaea9c47c6e4ecdb7c4b7cd6b687a6
rrogers422 <Reagan422440@gmail.com>
2022-02-18 11:39:21 -0500

    refractored react-typical code**error

--- -a/src/PortfolioContainer/Home/Profile.js
+++ -a/src/PortfolioContainer/Home/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Typical from 'react-typical';
+// import Typical from "react-typical";
 
 export default function Profile() {
   return (
@@ -22,35 +22,72 @@ export default function Profile() {
           </div>
 
           <div className="profile-details-name">
-              <span className="primary-text">
-                  {" "}
-                  Hello, I'm <span className="highlighted-text">Reagan</span>
+            <span className="primary-text">
+              {" "}
+              Hello, I'm <span className="highlighted-text">Reagan</span>
+            </span>
+          </div>
+          <div className="profile-datails-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {/* <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Dev😜",
+                    1000,
+                    "Full Stack Developer💻",
+                    1000,
+                    "React Dev📱",
+                    1000,
+                    "Mother👩🏽",
+                    1000,
+                  ]}
+                /> */}
+              </h1>
+              <span className="profile-role-tagline">
+                pro-gram-mer(noun)-a person who fixed a problem you didnt know
+                you have in a way you dont undertsand.
               </span>
-              </div>
-              <div className="profile-datails-role">
-                  <span className="primary-text">
-                      {" "}
-                      <h1>
-                        <Typical/>
-                        loop={Infinity}
-                        steps{[
-                            "Enthusiastic Dev😜",
-                            1000,
-                            "Full Stack Developer💻",
-                            1000,
-                            "React Dev📱",
-                            1000,
-                            "Mother👩🏽‍🍼",
-                            1000,
-                        ]}
-                      </h1>
-                      <span className="profile-role-tagline">
-                          
-                      </span>
-                  </span>
+            </span>
           </div>
         </div>
       </div>
     </div>
   );
 }
+// import React from "react";
+// // import Typical from "react-typical";
+
+// export default function Profile() {
+//   return (
+//     <div className="profile-container">
+//       <div className="profile-parent">
+//         <div className="profile-details">
+//           <div className="colz">
+//             <a href="#">
+//               <i className="fa fa-facebook-square"> </i>
+//             </a>
+//             <a href="#">
+//               <i className="fa fa-instagram"> </i>
+//             </a>
+//             <a href="#">
+//               <i className="fa fa-youtube-square"> </i>
+//             </a>
+//             <a href="#">
+//               <i className="fa fa-twitter"> </i>
+//             </a>
+//           </div>
+
+//           <div className="profile-details-name">
+//             <span className="primary-text">
+//               {" "}
+//               Hello, I'm <span className="highlighted-text">Reagan</span>
+//             </span>
+         
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
